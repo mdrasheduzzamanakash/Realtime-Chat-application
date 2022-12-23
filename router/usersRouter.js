007bb9d7f659babd554bd9b1035453746a09b626
Md. Rasheduzzaman <mdrasheduzzamanakash@gmail.com>
2022-12-23 22:09:44 +0600

    add user related operations

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,11 +3,22 @@ const express = require("express");
 const router = express.Router();
 
 // internal imports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 
 // logic related routes
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
-
